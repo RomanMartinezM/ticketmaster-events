089c7503397bea364560113e7bf525c85b0ab263
ronmar <sigma4726spring@gmail.com>
2023-10-12 20:23:42 -0600

    First component

---
+++ components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+       <div>
+            <p>Eventos</p>
+            <input placeholder="Busca tu evento favorito"></input>
+       </div>
+    );
+};
+
+export default Navbar;
